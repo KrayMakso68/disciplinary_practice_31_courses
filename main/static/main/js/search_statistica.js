5e0f7bf60e9f0a77575372a4027c5bfb99ccecc6
KrayMakso68 <makson6803@gmail.com>
2023-06-08 17:47:20 +0300

    add chart and table in statistica page

--- main/static/main/js/search_statistica.js
+++ main/static/main/js/search_statistica.js
@@ -1,13 +1,18 @@
 var startDate;
 var endDate;
+var promotions;
+var punishments;
+var withdrawals;
+var all_notes;
 const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+
 $(document).ready(function () {
 
     var start = moment().subtract(29, 'days');
     var end = moment();
 
     function cb(start, end) {
-        $('#reportrange span').html(start.format('D/MM/Y') + ' \u2013 ' + end.format('D/MM/Y'));
+        $('#reportrange span').html(start.format('DD.MM.Y') + ' \u2013 ' + end.format('DD.MM.Y'));
         startDate = start;
         endDate = end;
     }
@@ -67,11 +72,18 @@ const sendSearchData = (start, end) => {
         url: '/statistica_search/',
         data: {
             'csrfmiddlewaretoken': csrf,
-            'startdate': moment(start).format('D/MM/Y'),
-            'enddate': moment(end).format('D/MM/Y'),
+            'startdate': moment(start).format('YYYY-MM-DD'),
+            'enddate': moment(end).format('YYYY-MM-DD'),
         },
         success: (data) => {
             console.log(data);
+            promotions = data.promotions;
+            punishments = data.punishments;
+            withdrawals = data.withdrawals;
+            all_notes = data.all_notes;
+            chart.data.datasets[0].data = [data.promotions, data.punishments, data.withdrawals]
+            chart.update();
+            $('#content_row').replaceWith(`<tr class="text-center text-nowrap" id="content_row"><td>${startDate.format('DD.MM.Y')} \u2013 ${endDate.format('DD.MM.Y')}</td><th scope="row">${all_notes}</th><td>${promotions}</td><td>${punishments}</td><td>${withdrawals}</td></tr>`);
         },
         error: () => {
 
@@ -80,6 +92,44 @@ const sendSearchData = (start, end) => {
 }
 $('#search_btn').click(function(e){
     e.preventDefault();
-    console.log(startDate.format('D.MM.Y') + ' \u2013 ' + endDate.format('D.MM.Y'));
     sendSearchData(startDate, endDate);
 });
+
+const ctx = document.getElementById('myChart');
+
+        const data = {
+            labels: ['Поощрения', 'Взыскания', 'Снятия взыскания'],
+            datasets: [
+                {
+                    data: [1,1,1],
+                    backgroundColor: ['#27AE60', '#CB4335', '#faeee4'],
+                }
+            ]
+        };
+        const config = {
+          type: 'doughnut',
+          data: data,
+          options: {
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+            }
+          },
+        };
+        var chart = new Chart(ctx, config);
+
+        function handleHover(evt, item, legend) {
+            legend.chart.data.datasets[0].backgroundColor.forEach((color, index, colors) => {
+                colors[index] = index === item.index || color.length === 9 ? color : color + '4D';
+            });
+            legend.chart.update();
+        }
+
+        function handleLeave(evt, item, legend) {
+            legend.chart.data.datasets[0].backgroundColor.forEach((color, index, colors) => {
+                colors[index] = color.length === 9 ? color.slice(0, -2) : color;
+            });
+            legend.chart.update();
+        }
